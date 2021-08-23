0faace12b7631cc16b79c98f88abdb3fe7a179c3
Pedro Carranza <pdbcarranza@gmail.com>
2021-08-23 10:43:20 -0300

    Fixing Table

--- frontend/src/components/SideNav.js
+++ frontend/src/components/SideNav.js
@@ -122,7 +122,7 @@ class SideNav extends React.Component {
                         data={this.state.data}
                         options={{
                             cellStyle: {
-                                fontSize: 11,
+                                fontSize: 9,
                             },
                             toolbar: false,
                             sorting: false,
